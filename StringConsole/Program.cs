9722bc86404dded852c63591dbc2cdf7b56f6012
Holly Edwards-Kiss <hedwar15@my.centennialcollege.ca>
2026-04-09 14:34:34 -0400

    Initial commit - StringLibrary and console app

---
+++ StringConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using StringLibrary;
+
+namespace StringConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] samples = new[]
+            {
+                "Hello",
+                "world",
+                "",
+                "123",
+                "Uppercase",
+                "lowercase"
+            };
+
+            foreach (var sample in samples)
+            {
+                bool startsWithUpper = sample.StartsWithUpper();
+                Console.WriteLine(
+                    $"\"{sample}\" starts with uppercase? {startsWithUpper}");
+            }
+        }
+    }
+}
---
+++ StringLibrary/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            char firstChar = input[0];
+
+            return char.IsUpper(firstChar);
+        }
+    }
+}
